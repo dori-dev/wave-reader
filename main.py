78f83ec90b2baebe203c1ef5a5524050ac94b57e
mohammad <mr.dori.dev@gmail.com>
2021-11-14 18:46:50 +0330

    read a source

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pyaudio import PyAudio
 import struct
 
 music_speed = 1.1
+stoping = (False, 100)
 
 wav = open("music.wav", "rb")
 
@@ -22,3 +23,13 @@ stream = p.open(format=p.get_format_from_width(bits_per_sample/8),
                 channels=num_channels,
                 rate=sample_rate,
                 output=True)
+
+data = wav.read(sample_rate)
+bit = 0
+while data:
+    stream.write(data)
+    if stoping[0] and bit == stoping[1]:
+        wav.seek(34)
+        bit = 0
+    data = wav.read(sample_rate)
+    bit += 1
