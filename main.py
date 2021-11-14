12d2adf7e369c78bc2aaab42500a56d685cea330
mohammad <mr.dori.dev@gmail.com>
2021-11-14 18:42:28 +0330

    read num_channels and sample rate

--- main.py
+++ main.py
@@ -3,3 +3,11 @@
 
 from pyaudio import PyAudio
 import struct
+
+music_speed = 1.1
+
+wav = open("music.wav", "rb")
+
+wav.seek(22)
+num_channels = struct.unpack('<h', wav.read(2))[0]
+sample_rate = int(struct.unpack('<l', wav.read(4))[0] * music_speed)
